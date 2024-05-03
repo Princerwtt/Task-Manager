8644818b89464a6581d0f0897eaf5783143dfc29
princerwtt <princerawatnz@gmail.com>
2024-05-03 17:59:48 +0530

    done task

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,58 +1,43 @@
-
-
 import React, { useState, useEffect } from "react";
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+
 const Home = () => {
   const [tasks, setTasks] = useState([]);
   const [editTask, setEditTask] = useState(null);
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
-    if (storedTasks) {
-      setTasks(storedTasks);
-    }
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
   }, []);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const addTask = (task) => {
-    const newTasks = [...tasks, { ...task, id: Date.now() }];
-    setTasks(newTasks);
+  const addOrUpdateTask = (task) => {
+    const updatedTasks = editTask
+      ? tasks.map((t) => (t.id === editTask.id ? task : t))
+      : [...tasks, { ...task, id: Date.now() }];
+    setTasks(updatedTasks);
+    setEditTask(null);
   };
 
   const deleteTask = (id) => {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    setTasks(updatedTasks);
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
-const editTaskHandler = (task) => {
-  setEditTask(task);
-};
+  const editTaskHandler = (task) => {
+    setEditTask(task);
+  };
 
-const updateTask = (updatedTask) => {
-  const updatedTasks = tasks.map((task) =>
-    task.id === updatedTask.id ? updatedTask : task
-  );
-  setTasks(updatedTasks);
-  setEditTask(null);
-console.log(tasks)
-
-};
   return (
     <div className="container">
       <h1>Task Manager</h1>
-      {/* <Home /> This will render the Home component */}
       <div className="task-form">
-        <TaskForm onSubmit={editTask ? updateTask : addTask} />
+        <TaskForm onSubmit={addOrUpdateTask} editTask={editTask} />
       </div>
-      <TaskList
-        tasks={tasks}
-        onDelete={deleteTask}
-        onEdit={editTaskHandler}
-      />
+      <TaskList tasks={tasks} onDelete={deleteTask} onEdit={editTaskHandler} />
     </div>
   );
 };
